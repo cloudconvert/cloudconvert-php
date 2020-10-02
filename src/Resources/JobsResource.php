aa584ed21ae7b1181b88f7970556994d38e7543f
Josias Montag <josias@montag.info>
2020-10-02 12:54:23 +0200

    fix $job->refresh() method without query

--- src/Resources/JobsResource.php
+++ src/Resources/JobsResource.php
@@ -68,7 +68,7 @@ class JobsResource extends AbstractResource
      */
     public function refresh(Job $job, $query = null): Job
     {
-        $response = $this->httpTransport->get($this->httpTransport->getBaseUri() . '/jobs/' . $job->getId(), $query);
+        $response = $this->httpTransport->get($this->httpTransport->getBaseUri() . '/jobs/' . $job->getId(), $query ?? []);
         return $this->hydrator->hydrateObjectByResponse($job, $response);
     }
 
